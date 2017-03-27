408f0ab8a448a7dd7227019fafb575d96e7e6e7e
Martin Morães <martin.moraes@gmail.com>
2017-03-27 12:27:52 -0300

    System.exit(0); na principal.

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,16 +1,14 @@
 package principal;
 
-import java.io.IOException;
-
 import controle.ControleCarros;
 
 public class Principal {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		ControleCarros conexaoCarro = new ControleCarros();
 		conexaoCarro.executa(conexaoCarro.getConfiguracoesConexoes());
-		
+		System.exit(0);
 	}
 
 }
